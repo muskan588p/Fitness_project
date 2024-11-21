602c62a8284d5305ff24eebef23866123f22f004
muskan588p <musk2806@gmail.com>
2024-11-21 09:52:44 +0530

    database connect and views added

--- server/server.js
+++ server/server.js
@@ -1,20 +1,32 @@
-const express=require("express");
-const bcrypt=require("bcrypt");
-const path=require("path");
-const jwt=require("jsonwebtoken");
-const dotenv=require("dotenv");
-const connectDb=require("../server/config/dbConnection");
-
+const express = require('express');
+const path = require('path');
+const app = express();
+const hbs=require('hbs');
+const port = process.env.PORT || 5000;
+const connectDb = require("./config/dbConnection");
+const dotenv = require("dotenv");
 dotenv.config();
 
 connectDb();
 
+// const static_path=path.join(__dirname, "../client");
+const template_path=path.join(__dirname, "./templates/views");
+const partials_path=path.join(__dirname, "./templates/partials");
 
+// app.use(express.static(static_path));  //isse main index.hmtl khulgyi
 
+app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
+// app.get('/',(req,res)=>{
+//     res.send("working");
+// });
 
-const app=express();
-const port= 6000;
+console.log(template_path);
+app.get('/',(req,res)=>{
+    res.render("login");
+});
 
-app.listen(port,()=>{
-    console.log(`server running on port ${port}`);
-})
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
